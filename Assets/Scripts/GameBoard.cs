95299a96fa2ea6c9457c02d407c7fdd3d5965319
Memellod <shkolnik98@mail.ru>
2020-08-09 21:04:44 +0500

    Playable game, refactored code

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+///
+public class GameBoard : MonoBehaviour
+{
+    [Range(0,15)]
+    [SerializeField] int columns = 10, rows = 4; // number of cols and rows in board
+    [SerializeField] GameObject tileBase = null; // base of tile
+    Tile[,] board;          
+    public Tile selectedTile;                   
+    [SerializeField] int  scaleColumns = 2, scaleRows = 2;  // scale for calculating position
+    [SerializeField] GameObject panel;  // UI element (parent to gameBoard)
+
+    /// <summary>
+    /// calculates position for given row and column in board matrix
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <returns></returns>
+    private Vector3 Position(int row, int column)
+    {
+        return new Vector3(column * scaleRows, row * scaleColumns, 0);
+    }
+
+
+    private void Awake()
+    {
+        selectedTile = null;
+        board = new Tile[rows, columns];
+        GenerateBoard();
+        PlaceCells();
+    }
+
+    /// <summary>
+    /// placing tiles to its cells
+    /// </summary>
+    void PlaceCells()
+    {
+        Vector3 position;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (board[i, j] != null)
+                {
+                    position = Position(i, j);
+                    board[i, j].PlaceInCell(position, i, j);
+                }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// fills empty cells with random tiles
+    /// </summary>
+    void GenerateBoard()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (board[i, j] == null)
+                {
+                    GameObject newTileGO = Instantiate(tileBase);
+                    Tile newTile = newTileGO.GetComponent<Tile>();
+                    newTileGO.transform.parent = gameObject.transform;
+
+                    newTile.RandomizeTile();
+                    newTile.row = i;
+                    newTile.column = j;
+                    board[i, j] = newTile;
+
+                    // while there is match with that type of tile - randomize it
+                    while (Find3StraightMatch(newTile.row, newTile.column))
+                    {
+                        newTile.RandomizeTile();
+                    }
+
+                    // after defining type of tile - put in board
+
+                    board[i, j] = newTile;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// returns list of tiles that makes adjacent to match
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <returns></returns>
+    List<Tile> FindMatch(int row, int column)
+    {
+        List<Tile> answer = new List<Tile>();
+        // find 3-match to any direction 
+        if (Find3StraightMatch(row, column))
+        {
+            // if found, find every adjacent block and return list
+            Queue<Tile> queue = new Queue<Tile>();
+
+            queue.Enqueue(board[row, column]);
+            answer.Add(board[row, column]);
+            while (queue.Count > 0)
+            {
+                // getting object from queue
+                Tile tile = queue.Dequeue();
+                // getting adjacent Tiles of it
+                List<Tile> adjacent = GetAdjacentTiles(tile.row, tile.column);
+                // removing intersection (removing checked tiles) and @bad@ tiles
+                adjacent.RemoveAll(x => !x.IsSame(answer[0]) || answer.Contains(x));
+                // adding to set of result
+                answer.AddRange(adjacent);
+                // add to queue all unchecked tiles
+                adjacent.ForEach(x => queue.Enqueue(x));
+            }
+        }
+        return answer;
+    }
+
+    /// <summary>
+    /// returns bool indication that theres is 3 match for given row and column
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <returns></returns>
+    private bool Find3StraightMatch(int row, int column)
+    {
+        bool answer = false;
+        Tile tile = board[row, column];
+        
+
+        // if 2 up
+        if (row < rows - 2 
+            && board[row + 1, column] != null && board[row + 2, column] != null)
+        {
+            answer = board[row + 1, column].IsSame(tile) && board[row + 2, column].IsSame(tile);
+            if (answer)
+            {
+                return true;
+            }
+        }
+
+        // centered up and down
+        if (row < rows - 1 && row > 0 
+            && board[row + 1, column] != null && board[row - 1, column] != null)
+        {
+            answer = board[row + 1, column].IsSame(tile) && board[row - 1, column].IsSame(tile);
+            if (answer)
+            {
+                return true;
+            }
+        }
+
+        // 2 right
+        if (column < columns - 2 
+            && board[row, column + 1] != null && board[row, column + 2] != null)
+        {
+            answer = board[row, column + 1].IsSame(tile) && board[row, column + 2].IsSame(tile);
+            if (answer)
+            {
+                return true;
+            }
+        }
+
+        // centered left and right
+        if (column < columns - 1 && column > 0
+            && board[row, column + 1] != null && board[row, column - 1] != null)
+        {
+            answer = board[row, column + 1].IsSame(tile) && board[row, column - 1].IsSame(tile);
+            if (answer) 
+            {
+                return true;
+            }
+        }
+
+        // 2 lower
+        if (row > 1 
+            && board[row - 1, column] != null && board[row - 2, column] != null)
+        {
+            answer = board[row - 1, column].IsSame(tile) && board[row - 2, column].IsSame(tile);
+            if (answer)
+            {
+                return true;
+            }
+        }
+
+        // 2 left
+        if (column > 1
+            && board[row, column - 1] != null && board[row, column - 2] != null)
+        {
+            answer = board[row, column - 1].IsSame(tile) && board[row, column - 2].IsSame(tile);
+            if (answer)
+            {
+                return true;
+            }
+        }
+
+        // no matches found
+        return false;
+    }
+
+
+    /// <summary>
+    /// returns list of adjacent tiles
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <returns></returns>
+    private List<Tile> GetAdjacentTiles(int row, int column)
+    {
+        List<Tile> answer = new List<Tile>();
+
+        // add down
+        if (row != 0 
+            && board[row - 1, column] != null) answer.Add(board[row - 1, column]);
+
+        // add up
+        if (row != rows - 1
+            && board[row + 1, column] != null) answer.Add(board[row + 1, column]);
+
+        // add left
+        if (column != 0
+            && board[row, column - 1] != null) answer.Add(board[row, column - 1]);
+
+        // add right
+        if (column != columns - 1
+            && board[row, column + 1] != null) answer.Add(board[row, column + 1]);
+
+
+        return answer;
+    }
+
+
+    /// <summary>
+    ///  try to swap tiles
+    /// </summary>
+    /// <param name="tileToSwap"></param>
+    public void TrySwapTiles(Tile tileToSwap)
+    {
+
+        // if tile is too far - do nothing
+        if (!GetAdjacentTiles(tileToSwap.row, tileToSwap.column).Contains(selectedTile)) return;
+
+        board[selectedTile.row, selectedTile.column] = tileToSwap;
+        board[tileToSwap.row, tileToSwap.column] = selectedTile;
+
+        PlaceCells();
+
+        List<Tile> matchedTiles = FindMatch(selectedTile.row, selectedTile.column);
+        if (matchedTiles.Count > 2)
+        {
+            Debug.Log("Is a match!!! on " + selectedTile.row + " row and " + selectedTile.column + " column");
+            ExplodeTiles(matchedTiles);
+        }
+
+        matchedTiles = FindMatch(tileToSwap.row, tileToSwap.column);
+        if (matchedTiles.Count > 2)
+        {
+
+            Debug.Log("Is a match!!!" + tileToSwap.row + " row and " + tileToSwap.column + " column");
+            ExplodeTiles(matchedTiles);
+        }
+
+        do
+        {
+            DescendCells();
+            GenerateBoard();
+            PlaceCells();
+        }
+        while (CheckForMatchForEveryTile());
+
+
+        // after this no  more matches possible
+
+
+        selectedTile.OnDeselected();
+        selectedTile = null;
+    }
+
+
+    /// <summary>
+    ///  checks for matches for every tile (after descending for ex.)
+    /// </summary>
+    /// <returns></returns>
+    private bool CheckForMatchForEveryTile()
+    {
+        bool answer = false;
+        bool flag;
+        do
+        {
+            flag = false;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (board[i, j] != null)
+                    {
+                        List<Tile> matchedTiles = FindMatch(i, j);
+                        if (matchedTiles.Count > 2)
+                        {
+                            flag = true;
+                            answer = true;
+                            ExplodeTiles(matchedTiles);
+                        }
+                    }
+                }
+            }
+        } while (flag);
+        return answer;
+    }
+
+
+
+    /// <summary>
+    ///  delete matched tiles and gain points according to number of tiles exploded
+    /// </summary>
+    /// <param name="matchedTile"></param>
+    private void ExplodeTiles(List<Tile> matchedTile)
+    {
+        // get points for matching
+       // int points = (int)(matchedTile.Count * 40 + Mathf.Pow(2, matchedTile.Count) * 10);
+
+        // send points to GameManager???
+        // gameManager.AddPoints(points);
+        // 
+        
+        foreach (Tile i in matchedTile)
+        {
+            board[i.row, i.column] = null;
+            Destroy(i.gameObject);
+        }
+    }
+
+
+
+    /// <summary>
+    ///  descends tiles to empty positions
+    /// </summary>
+    private void DescendCells()
+    {
+        for (int i = 0; i < rows;  i++)                            // from 2nd row
+        {   
+            for (int j = 0; j < columns; j++)                    // through all columns
+            {
+                if (board[i, j] == null)                        // if lower is null then descend whole column
+                {
+                    int p = i;                    
+                    while (p < rows && board[p, j] == null)     // find first not-null in column and descend 
+                    {
+                        p++;
+                    }
+                    
+                    if (p != rows)                              // if found any - descend column to down
+                    {
+                        for (int k = 0; k < rows - p; k++)      // rows - p = number of descending tiles
+                        {
+                            board[i + k, j] = board[p + k, j];
+                            board[p + k, j] = null;
+                        }
+                    }
+                }
+            }
+        }
+
+        PlaceCells();
+
+    }
+
+
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour, IPointerClickHandler
+{
+    float scale = 0.75f;
+    public Sprite sprite;
+    Image image;
+    BoxCollider2D col;
+    public int row, column;
+
+    /// <summary>
+    /// randomezes type of tile
+    /// </summary>
+    public void RandomizeTile()
+    {
+        sprite = Constants.spriteList[Random.Range(0, Constants.spriteList.Length)];
+        name = sprite.name;
+    }
+
+    public void OnSelected()
+    {
+        image.color = Color.grey;
+    }
+    public void OnDeselected()
+    {
+        image.color = Color.white;
+    }
+
+
+    /// <summary>
+    /// checks if this tile and provided are same
+    /// </summary>
+    /// <param name="tile"></param>
+    /// <returns></returns>
+    public bool IsSame(Tile tile)
+    {
+        return sprite == tile.sprite;
+    }
+
+    /// <summary>
+    /// placing tile in cell and defining its row and column
+    /// </summary>
+    /// <param name="localposition"></param>
+    /// <param name="_row"></param>
+    /// <param name="_column"></param>
+    public void PlaceInCell(Vector3 localposition, int _row, int _column)
+    {
+        transform.localPosition = localposition;
+        row = _row;
+        column = _column;
+        if (image == null)
+        {
+            image = gameObject.AddComponent<Image>();
+            image.sprite = sprite;
+
+            image.transform.localScale = Vector3.one * scale;
+        }
+        if (col == null)
+        {
+            col = gameObject.AddComponent<BoxCollider2D>();
+        }
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        if (Constants.gameBoard.selectedTile == null)           // no selected tile
+        {
+            OnSelected();
+            Constants.gameBoard.selectedTile = this;
+        }
+        else
+        {
+            if (Constants.gameBoard.selectedTile == this)       // if clicking on same tile as selected
+            {
+                OnDeselected();
+                Constants.gameBoard.selectedTile = null;
+            }
+            else                                             // clicking on another tile 
+            {
+                Constants.gameBoard.TrySwapTiles(this);
+            }
+        }
+    }
+}
